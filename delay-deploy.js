da1879a3a444faad05215291cabc3d9f181e01a3
Dan Onoshko <d.onoshko.student@sibneuro.ru>
2014-08-27 23:45:11 +0700

    fixes

--- delay-deploy.js
+++ delay-deploy.js
@@ -1,12 +1,13 @@
 "use strict";
 
-var COON = require("./coon.js");
+var coon = require("./coon.js");
 
 var args = process.argv;
 args.splice(0, 2);
 
-COON.gitSeeker(function(){
-    args[1] = JSON.parse(args[1]);
-    args[3] = JSON.parse(args[3]);
-    COON.deploy.apply({}, args);
+args[1] = JSON.parse(args[1]);
+args[3] = JSON.parse(args[3]);
+
+coon.gitSeeker(function(){
+    coon.deploy.apply({}, args);
 });
