0b7f4c4829ca7c8c5f9aa33f621992b9ecc4755a
Dan Onoshko <d.onoshko.student@sibneuro.ru>
2014-08-31 23:37:24 +0700

    test

--- delay-deploy.js
+++ delay-deploy.js
@@ -2,12 +2,12 @@
 
 var coon = require("./coon.js");
 
-var args = process.argv;
-args.splice(0, 2);
+//var args = process.argv;
+//args.splice(0, 2);
 
-args[1] = JSON.parse(args[1]);
-args[3] = JSON.parse(args[3]);
+//args[1] = JSON.parse(args[1]);
+//args[3] = JSON.parse(args[3]);
 
 coon.gitSeeker(function(){
-    coon.deploy.apply({}, args);
+    coon.deploy.apply({}, ["test", {}, "trigen", {}]);
 });
